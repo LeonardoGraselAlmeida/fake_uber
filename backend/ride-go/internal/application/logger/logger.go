5869329ebcd915f7202d7efa25e3e834f8b6768d
Leonardo Almeida <leonardograselalmeida@gmail.com>
2023-12-04 22:06:53 +0000

    remove go project

--- backend/ride-go/internal/application/logger/logger.go
+++
@@ -1,5 +0,0 @@
-package logger
-
-type LoggerInterface interface {
-	Log(message string)
-}
--- backend/ride-go/internal/infra/logger/logger.go
+++
@@ -1,7 +0,0 @@
-package logger
-
-type Logger struct{}
-
-func (l *Logger) Log(message string) {
-	println(message)
-}
